c62e403f610b63d7bd4b9b6cf425aa2d19f5c682
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-27 15:16:24 +0100

    secound-end

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+// main.js // enterTaste mit Aktion Button verbinden
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            checkData();
+        }
+    });
+});
+
+
+
+function checkData() {
+    // Überprüfe, ob die Auswahl für Geschlecht getroffen wurde
+    let geschlecht;
+    if (document.getElementById('frau').checked) {
+        geschlecht = "weiblich";
+    } else if (document.getElementById('mann').checked) {
+        geschlecht = "männlich";
+    } else {
+        alert("");
+        return;
+    }
+
+    // Hole die eingegebenen Werte
+    let alter = parseInt(document.getElementById('input-age').value);
+    let gewicht = parseInt(document.getElementById('input-width').value);
+    let groesse = parseInt(document.getElementById('input-height').value);
+    let aktivitaetsLevel = document.getElementById('service').value;
+
+    // Überprüfe, ob alle Werte eingegeben wurden
+    if (isNaN(alter) || isNaN(gewicht) || isNaN(groesse) || aktivitaetsLevel === "") {
+        alert("Bitte fülle alle Felder aus.");
+        return;
+    }
+
+    // Berechne den Grundumsatz nach der Harris-Benedict-Formel
+    let grundumsatz;
+    if (geschlecht === "weiblich") {
+        grundumsatz = 655.1 + (9.6 * gewicht) + (1.8 * groesse) - (4.7 * alter);
+    } else {
+        grundumsatz = 66.47 + (13.7 * gewicht) + (5 * groesse) - (6.8 * alter);
+    }
+
+    // Berechne den Gesamtumsatz mit dem PAL-Faktor
+    let palFaktor;
+    switch (aktivitaetsLevel) {
+        case "faul":
+            palFaktor = 1.2;
+            break;
+        case "gut":
+            palFaktor = 1.375;
+            break;
+        case "mittel":
+            palFaktor = 1.55;
+            break;
+        case "sehr":
+            palFaktor = 1.725;
+            break;
+        case "super":
+            palFaktor = 1.9;
+            break;
+        default:
+            alert("Ungültiges Aktivitätslevel.");
+            return;
+    }
+
+    let gesamtumsatz = grundumsatz * palFaktor;
+
+    // Zeige die Ergebnisse im HTML an
+    document.getElementById('result-A').textContent = " Grundumsatz (Ohne Aktivitäts)kcal: " + grundumsatz.toFixed(0) + " pro Tag";
+    document.getElementById('resultTot-B').textContent = " Gesamtumsatz (inkl. Aktivität)kcal: " + gesamtumsatz.toFixed(0)+ " pro Tag"; 
+}
+
